d66707bc9d2ef93cfcf334eb4f58ea63f39d8582
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-11-22 02:18:25 +0530

    Added the Navbar for the dashboard

--- Payment-Transfer-App/Frontend/src/components/AppBar.jsx
+++ Payment-Transfer-App/Frontend/src/components/AppBar.jsx
@@ -1,15 +1,14 @@
-export const AppBar=()=>{
-    return <div className="shadow h-14 flex justify-between">
+export const AppBar = () => {
+    return <div className="shadow h-14 flex justify-between items-center">
         <div className="flex flex-col justify-center h-full ml-4">
             PayTM App
         </div>
-        <div className="flex">
-<div className="flex flex-col justify-center h-full mr-4">
-    <div className="flex flex-col justify-center h-full text-center">
-        U
-    </div>
-</div>
+        <div className="flex justify-center items-center mr-4">
+            <div className="flex items-center justify-center h-10 w-10 rounded-full bg-slate-200">
+                <div className="text-center">
+                    U
+                </div>
+            </div>
         </div>
-
     </div>
 }
