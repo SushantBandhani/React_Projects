93fa20570cecc95a86113f4ea5adfcba0685303b
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-11-22 02:17:32 +0530

    Added Dashboard page

--- Payment-Transfer-App/Frontend/src/pages/Dashboard.jsx
+++ Payment-Transfer-App/Frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,11 @@
 import { AppBar } from "../components/AppBar"
+import { MessageComponent } from "../components/MessageComponent"
+import { InputComponent } from "../components/InputComponent"
 
 export const Dashboard=()=>{
     return <>
     <AppBar></AppBar>
+    <MessageComponent></MessageComponent>
+    <InputComponent></InputComponent>
     </>
 }
