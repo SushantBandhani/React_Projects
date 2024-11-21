90c24235da4a25245fd483ae2c8617f55fd00ba4
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-11-21 10:27:28 +0530

    Adding the AppBar component

---
+++ Payment-Transfer-App/Frontend/src/components/AppBar.jsx
@@ -0,0 +1,15 @@
+export const AppBar=()=>{
+    return <div className="shadow h-14 flex justify-between">
+        <div className="flex flex-col justify-center h-full ml-4">
+            PayTM App
+        </div>
+        <div className="flex">
+<div className="flex flex-col justify-center h-full mr-4">
+    <div className="flex flex-col justify-center h-full text-center">
+        U
+    </div>
+</div>
+        </div>
+
+    </div>
+}
