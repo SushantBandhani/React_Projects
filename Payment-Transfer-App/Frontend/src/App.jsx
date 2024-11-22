f6ec7c7f800a1a7f206be753f10405b69b341a6c
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-11-22 11:59:05 +0530

    Updating the main page file and adding additonal send component

--- Payment-Transfer-App/Frontend/src/App.jsx
+++ Payment-Transfer-App/Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import {Signup} from './pages/Signup'
 import {Signin} from './pages/Signin'
 import {Dashboard} from './pages/Dashboard'
-// import {Send} from './pages/Send'
+import {Send} from './pages/Send'
 function App() {
 
   return (
@@ -12,7 +12,7 @@ function App() {
     <Route path="/signup" element={<Signup></Signup>}></Route>
     <Route path="/signin" element={<Signin></Signin>}></Route>
     <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
-    {/* <Route path="/send" element={<Send></Send>}></Route> */}
+    <Route path="/send" element={<Send></Send>}></Route>
    </Routes>
    </BrowserRouter>
    </>
