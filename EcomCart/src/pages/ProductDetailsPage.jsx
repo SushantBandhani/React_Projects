37bf513d433f39e8bc99ae48a1ad58a0a3a7d913
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-12-04 10:51:55 +0530

    Added product details pages

---
+++ EcomCart/src/pages/ProductDetailsPage.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../navbar/Navbar";
+import ProductDetails from "../product-List/components/ProductDetails";
+
+export default function ProdcutDetailsPage(){
+    return(
+        <div>
+            <Navbar>
+                <ProductDetails></ProductDetails>
+            </Navbar>
+        </div>
+    )
+}
