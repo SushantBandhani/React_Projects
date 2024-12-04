908857ac74999edb26cf685ca367506ad17019d4
Sushant Bandhani <sushantbandhani22@gmail.com>
2024-12-04 10:23:29 +0530

    Introducing suspense

--- EcomCart/src/pages/Home.jsx
+++ EcomCart/src/pages/Home.jsx
@@ -1,11 +1,17 @@
+import { RecoilRoot } from "recoil";
+import React, { Suspense } from 'react';
 import Navbar from "../navbar/Navbar";
-import ProductList from "../product-List/ProductList";
+import ProductList from "../product-List/components/ProductList";
 
 export default function Home(){
     return(
         <div>
             <Navbar>
-                <ProductList></ProductList>
+                <RecoilRoot>
+                    <Suspense fallback={<div>Loading.....</div>}>
+                    <ProductList></ProductList>
+                    </Suspense>
+                </RecoilRoot>
             </Navbar>
         </div>
     )
