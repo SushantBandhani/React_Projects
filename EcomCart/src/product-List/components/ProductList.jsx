46c0b11a0f859c552f7af039c06fefa364dda75b
sushant <sushantbandhani22@gmail.com>
2024-12-11 11:13:31 +0530

    Added pagination

--- EcomCart/src/product-List/components/ProductList.jsx
+++ EcomCart/src/product-List/components/ProductList.jsx
@@ -19,6 +19,7 @@ import {
 } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
+import {Items_Per_Page} from '../../app/constants'
 
 // section represents the parts and options represents values inside respective partions
 
@@ -134,14 +135,36 @@ export default function ProductList() {
   const setsortArr = useSetRecoilState(sortData);
   const setProducts = useSetRecoilState(productsState);
   const setStatus = useSetRecoilState(statusState);
+  const [totalItems,setTotalItems]=useState(0);
+  // const [page,setPage]=useState(1);
+  let _limit=10;
+  // let totalItems=0;
+  const [page,setPage]=useState(1);
+  
+
+  function handlePage(page){
+    console.log(page)
+    setPage(page)
+  }
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setStatus('loading');
-        const response = await fetch('http://localhost:3000/products');
+        const response = await fetch(`https://dummyjson.com/products?limit=${_limit}&skip=${(page-1)*_limit}`);
         const data = await response.json();
-        setProducts(data);
+
+        /* Need to fix this headers issue to get the totalcount ,until then manually setting a totalItems */
+
+        // const tot=await response.headers.get("X-ratelimit-limit");
+        // console.log('X-Ratelimit-Limit:', response.headers.get('X-Ratelimit-Limit'));
+        // if(response.headers.get("X-ratelimit-limit")){
+        //   console.log("yes")
+        // }
+        // setTotalItems(tot)
+        // console.log("total in useEffect--> ",tot)
+        // console.log(data)
+        setProducts(data.products);
         setStatus('success');
       } catch (error) {
         console.error('Error fetching products:', error);
@@ -150,7 +173,7 @@ export default function ProductList() {
     };
 
     fetchData();
-  }, [setProducts, setStatus]);
+  }, [setProducts, setStatus,page]);
 
 
   const handleFilter = (e, section, option) => {
@@ -242,7 +265,7 @@ export default function ProductList() {
 
               {/* Section of Products ---------------------- */}
               <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-                <Pagination></Pagination>
+                <Pagination page={page} setPage={setPage} handlePage={handlePage} totalItems={100}></Pagination>
               </div>
             </main>
           </div>
@@ -365,7 +388,8 @@ function DesktopFilter({ handleFilter }) {
   </form>;
 }
 
-function Pagination() {
+function Pagination({page,setPage,handlePage,totalItems}) {
+  // console.log("Total --> ",totalItems)
   return (
     <>
       <div className="flex flex-1 justify-between sm:hidden">
@@ -385,8 +409,8 @@ function Pagination() {
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700">
-            Showing <span className="font-medium">1</span> to <span className="font-medium">10</span> of{' '}
-            <span className="font-medium">97</span> results
+            Showing <span className="font-medium">{(page-1)*Items_Per_Page+1}</span> to <span className="font-medium">{(page)*Items_Per_Page>totalItems?totalItems:(page)*Items_Per_Page}</span> of{' '}
+            <span className="font-medium">{totalItems}</span> results
           </p>
         </div>
         <div>
@@ -399,14 +423,19 @@ function Pagination() {
               <ChevronLeftIcon aria-hidden="true" className="size-5" />
             </a>
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-            <a
-              href="#"
-              aria-current="page"
-              className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              1
-            </a>
-            <a
+
+            {Array.from({length:Math.ceil(totalItems/Items_Per_Page)}).map((ele,index)=>{
+        
+  return  <div
+    onClick={(e)=>handlePage(index+1)}
+    aria-current="page"
+    className={`relative cursor-pointer z-10 inline-flex items-center ${index+1===page?'bg-indigo-600 text-white':'text-gray-900'} px-4 py-2 text-sm font-semibold  focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+  >
+    {index+1}
+  </div>
+            })}
+        
+            {/* <a
               href="#"
               className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
@@ -439,13 +468,14 @@ function Pagination() {
             >
               10
             </a>
+            */
             <a
               href="#"
               className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Next</span>
               <ChevronRightIcon aria-hidden="true" className="size-5" />
-            </a>
+            </a> }
           </nav>
         </div>
       </div>
