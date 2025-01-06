242f18cfc5dbf1b56fd171402c1d8195d1674855
sushant <sushantbandhani22@gmail.com>
2025-01-06 12:20:34 +0530

    Intializing the App component and routers

---
+++ Medium-Clone/Frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import {Signup} from './pages/Signup'
+import {Signin} from './pages/Signin'
+import {Blog} from './pages/Blog'
+
+import './App.css'
+import { Blogs } from './pages/Blogs'
+
+function App() {
+
+  return (
+         <>
+         <BrowserRouter>
+         <Routes>
+          <Route path="/signup" element={<Signup></Signup>}></Route>
+          <Route path="/signin" element={<Signin></Signin>}></Route>
+          <Route path="/blog/:id" element={<Blog></Blog>}></Route>
+          <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+         </Routes>
+         </BrowserRouter>
+         </>
+  )
+}
+
+export default App
