9c5d572e0e64517f9f48f71ac89e11cb2ac3aea2
sushant <sushantbandhani22@gmail.com>
2025-01-07 11:42:28 +0530

    Added skeletal loader and spinner

--- Medium-Clone/Frontend/src/pages/Blogs.tsx
+++ Medium-Clone/Frontend/src/pages/Blogs.tsx
@@ -1,17 +1,26 @@
 import { Appbar } from "../components/Appbar";
 import { BlogCard } from "../components/BlogCard"
+import { BlogSkeleton } from "../components/BlogSkeleton";
 import { useBlogs } from "../hooks";
 
 
 export const Blogs=()=>{
     const { loading, blogs } = useBlogs();
 
-    if(loading){
+    if (loading) {
         return <div>
-            loading...
+            <Appbar /> 
+            <div  className="flex justify-center">
+                <div>
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                </div>
+            </div>
         </div>
     }
-
     return <div>
     <Appbar />
     <div  className="flex justify-center">
