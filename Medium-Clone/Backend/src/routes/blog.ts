253013eacc652d69a37be147c85b4a66756e90bf
sushant <sushantbandhani22@gmail.com>
2025-01-07 11:51:19 +0530

    Added cors and updated bulk and signup endpoint

--- Medium-Clone/Backend/src/routes/blog.ts
+++ Medium-Clone/Backend/src/routes/blog.ts
@@ -51,11 +51,21 @@ blogRouter.get('/bulk', async (c) => {
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
 
-  const blogs = await prisma.post.findMany();
+  const blogs = await prisma.post.findMany({
+    select:{
+      content:true,
+      title:true,
+      id:true,
+      author:{
+        select:{
+          name:true
+        }
+      }
+    }
+  });
 
   return c.json({ blogs });
 })
-
 blogRouter.get('/:id', async (c) => {
   const id = c.req.param("id")
   const prisma = new PrismaClient({
@@ -65,6 +75,17 @@ blogRouter.get('/:id', async (c) => {
     const blog = await prisma.post.findFirst({
       where: {
         id: id
+      },
+      select:{
+        id:true,
+        title:true,
+        content:true,
+        author:{
+          select:{
+            name:true
+          }
+        }
+
       }
     })
     return c.json({ blog })
