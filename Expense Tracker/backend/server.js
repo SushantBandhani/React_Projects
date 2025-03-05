58d4c3342540ec334c356fbd9ab36d47c71df1fb
sushant <sushantbandhani22@gmail.com>
2025-03-05 07:50:55 +0530

    Added model and routes to get income related queries and completed excel file download feature

--- Expense Tracker/backend/server.js
+++ Expense Tracker/backend/server.js
@@ -4,6 +4,7 @@ const cors=require("cors")
 const path=require("path")
 const connectDB=require('./config/db')
 const authRoutes=require('./routes/authRoutes')
+const incomeRoutes=require('./routes/incomeRoutes')
 
 const app=express()
 
@@ -16,9 +17,13 @@ app.use(cors({
 }))
 
 app.use(express.json())
+
+//server uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 connectDB();
 
 app.use("/api/v1/auth",authRoutes)
+app.use("/api/v1/income",incomeRoutes)
 
 const PORT=process.env.PORT || 5000;
 
