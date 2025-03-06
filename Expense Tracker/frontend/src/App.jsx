093935535214c76e2d4c6ebe75bd122623743ecc
sushant <sushantbandhani22@gmail.com>
2025-03-06 08:40:35 +0530

    Added context api and updated App.jsx

--- Expense Tracker/frontend/src/App.jsx
+++ Expense Tracker/frontend/src/App.jsx
@@ -1,26 +1,35 @@
 import react from 'react'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import {BrowserRouter as Router, Routes,Route,Navigate} from "react-router-dom"
 import Login from './pages/Auth/Login'
 import Signup from './pages/Auth/Signup'
 import Home from './pages/Dashboard/Home'
 import Income from './pages/Dashboard/Income'
 import Expense from './pages/Dashboard/Expense'
+import UserProvider from './context/useContext';
 
-const App=()=>{
-  return <div>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Root />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/signUp" element={<Signup />}></Route>
-        <Route path="/dashboard" element={<Home />}></Route>
-        <Route path="/income" element={<Income />}></Route>
-        <Route path="/expense" element={<Expense />}></Route>
-      </Routes>
-
-    </Router>
-  </div>
-}
+const App = () => {
+  return (
+    <>
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
+      <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<Signup />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+        </Routes>
+      </Router>
+      </UserProvider>
+     
+    </>
+  );
+};
 
 export default App
 
