20a9e12863aa220b7823da306dbd167ed91ce36e
sushant <sushantbandhani22@gmail.com>
2025-03-07 07:40:07 +0530

    Corrected textual error in apiPaths and added data in data.js

--- Expense Tracker/frontend/src/utils/apiPaths.js
+++ Expense Tracker/frontend/src/utils/apiPaths.js
@@ -6,7 +6,7 @@ export const API_PATHS={
     AUTH:{
         LOGIN:"/api/v1/auth/login",
         REGISTER:"/api/v1/auth/register",
-        GET_USER_ID:"/api/v1/auth/getUser",
+        GET_USER_INFO:"/api/v1/auth/getUser",
     },
     DASHBOARD:{
         GET_DATA:"/api/v1/dashboard"
