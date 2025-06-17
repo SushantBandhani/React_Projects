1618f3ecc5871260eef05742f43d3b0c10c160dd
Sushant Bandhani <101317113+SushantBandhani@users.noreply.github.com>
2025-06-17 06:04:55 +0530

    Adding react hot toast

--- Expense Tracker/frontend/src/App.jsx
+++ Expense Tracker/frontend/src/App.jsx
@@ -1,41 +1,36 @@
-import react from 'react'
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-import {BrowserRouter as Router, Routes,Route,Navigate} from "react-router-dom"
-import Login from './pages/Auth/Login'
-import Signup from './pages/Auth/Signup'
-import Home from './pages/Dashboard/Home'
-import Income from './pages/Dashboard/Income'
-import Expense from './pages/Dashboard/Expense'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from './pages/Auth/Login';
+import Signup from './pages/Auth/Signup';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
 import UserProvider from './context/useContext';
 import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
     <>
-      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
       <UserProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Root />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signUp" element={<Signup />} />
-          <Route path="/dashboard" element={<Home />} />
-          <Route path="/income" element={<Income />} />
-          <Route path="/expense" element={<Expense />} />
-        </Routes>
-      </Router>
-      <Toaster
-      toastOptions={{
-        className:"",
-        style:{
-          fontSize:'13px'
-        }
-      }}
-      ></Toaster>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Root />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signUp" element={<Signup />} />
+            <Route path="/dashboard" element={<Home />} />
+            <Route path="/income" element={<Income />} />
+            <Route path="/expense" element={<Expense />} />
+          </Routes>
+        </Router>
+        <Toaster
+          position="top-center" // ← This controls the position
+          toastOptions={{
+            style: {
+              fontSize: '13px',
+            },
+          }}
+        />
       </UserProvider>
-     
     </>
   );
 };
@@ -43,10 +38,10 @@ const App = () => {
 export default App
 
 
-const Root=()=>{
+const Root = () => {
   // Check if token exist in local storage
-  const isAuthenticated=!!localStorage.getItem("token");
+  const isAuthenticated = !!localStorage.getItem("token");
 
   // Redirect to dashboard if aurthenticated otherwise to login
-  return isAuthenticated?(<Navigate to="/dashboard" />) : (<Navigate to="login" />)
+  return isAuthenticated ? (<Navigate to="/dashboard" />) : (<Navigate to="login" />)
 }
