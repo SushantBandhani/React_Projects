003af75831eb4e95d9652795f7063a5d5341e26e
Sushant Bandhani <101317113+SushantBandhani@users.noreply.github.com>
2025-08-20 20:27:02 +0530

    Refactor SideMenu component for readability

--- Expense Tracker/frontend/src/components/layouts/SideMenu.jsx
+++ Expense Tracker/frontend/src/components/layouts/SideMenu.jsx
@@ -1,55 +1,64 @@
-import { SIDE_MENU_DATA } from "@/utils/data"
+import { SIDE_MENU_DATA } from "@/utils/data";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "@/context/useContext";
 import CharAvatar from "@/Cards/CharAvatar";
 
-const SideMenu=({activeMenu})=>{
-const {user,clearUser}=useContext(UserContext);
-const navigate=useNavigate();
+const SideMenu = ({ activeMenu }) => {
+  const { user, clearUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
-const handleClick=(route)=>{
-    if(route==="logout"){
-        handleLogout();
-        return;
+  const handleClick = (route) => {
+    if (route === "logout") {
+      handleLogout();
+      return;
     }
-    navigate(route)
-}
+    navigate(route);
+  };
 
-    const handleLogout=()=>{
-        localStorage.clear();
-        clearUser();
-        navigate("/login")
-    }
+  const handleLogout = () => {
+    localStorage.clear();
+    clearUser();
+    navigate("/login");
+  };
 
-    return (
-        <div className="w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-200/50 p-5 sticky top-[61px]">
-            <div className="flex flex-col items-center justify-center gap-3 mt-3 mb-7">
-                {user?.profileImageUrl?(
-                     <img src={user?.profileImageUrl || ""}
-                     alt="Profile Image"
-                     className="w-20 h-20 bg-slate-400 rounded-full"
-                     ></img>
-                ):(<CharAvatar fullName={user?.fullName} width='w-20' height="h-20" style="text-xl"></CharAvatar>)}
-                <h5 className="text-gray-950 font-medium leading-6">
-                    {user?.fullName || ""}
-                </h5>
-            </div>
-            {SIDE_MENU_DATA.map((item, index) => (
-    <button
-        key={`menu_${index}`}
-        className={`w-full flex items-center gap-4 text-[15px] ${activeMenu === item.label ? "text-white bg-violet-500" : ""} py-3 px-6 rounded-lg mb-3`}
-        onClick={()=>{
-            handleClick(item.path)
-        }}
-    >
-        <item.icon className="text-xl"/>
-        {item.label}
-    </button>
-))}
+  return (
+    <div className="w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-200/50 p-5 sticky top-[61px]">
+      <div className="flex flex-col items-center justify-center gap-3 mt-3 mb-7">
+        {user?.profileImageUrl ? (
+          <img
+            src={user?.profileImageUrl || ""}
+            alt="Profile Image"
+            className="w-20 h-20 bg-slate-400 rounded-full"
+          ></img>
+        ) : (
+          <CharAvatar
+            fullName={user?.fullName}
+            width="w-20"
+            height="h-20"
+            style="text-xl"
+          ></CharAvatar>
+        )}
+        <h5 className="text-gray-950 font-medium leading-6">
+          {user?.fullName || ""}
+        </h5>
+      </div>
+      {SIDE_MENU_DATA.map((item, index) => (
+        <button
+          key={`menu_${index}`}
+          className={`w-full flex items-center gap-4 text-[15px] ${
+            activeMenu === item.label ? "text-white bg-violet-500" : ""
+          } py-3 px-6 rounded-lg mb-3`}
+          onClick={() => {
+            handleClick(item.path);
+          }}
+        >
+          <item.icon className="text-xl" />
+          {item.label}
+        </button>
+      ))}
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default SideMenu
+export default SideMenu;
