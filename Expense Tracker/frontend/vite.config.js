0d8c7d59fc9c2ee021c485721d2b5108a40812d5
Sushant Bandhani <101317113+SushantBandhani@users.noreply.github.com>
2025-09-10 20:50:44 +0530

    Refactor AuthLayout and update vite config imports

--- Expense Tracker/frontend/vite.config.js
+++ Expense Tracker/frontend/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from "path"; // ✅ Import path (required)
+import path,{__dirname} from "path";
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),
-  // plugins: [react(),
   ],
   resolve: {
     alias: {
