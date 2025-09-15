b6070c70fabc285f4e1454a11253ce861e57adc3
Sushant Bandhani <101317113+SushantBandhani@users.noreply.github.com>
2025-09-15 17:04:08 +0530

    Refactor layout and helper files for consistency

--- Expense Tracker/frontend/src/components/layouts/DashboardLayout.jsx
+++ Expense Tracker/frontend/src/components/layouts/DashboardLayout.jsx
@@ -1,26 +1,26 @@
-import { UserContext } from "@/context/useContext"
-import { useContext } from "react"
-import Navbar from "./Navbar"
-import SideMenu from "./SideMenu"
+import { UserContext } from "@/context/useContext";
+import { useContext } from "react";
+import Navbar from "./Navbar";
+import SideMenu from "./SideMenu";
 
-const DashboardLayout=({children,activeMenu})=>{
-    const {user}=useContext(UserContext)
-    // What if it takse sometime to load a skeleton would be good
-    // if(!user) return <div>Loading...</div>
-    return (
-        <div className="">
-            <Navbar activeMenu={activeMenu}></Navbar>
-            {user &&(
-                <div className="flex">
-                    <div className="max-[1080px]:hidden">
-                        <SideMenu activeMenu={activeMenu}></SideMenu>
-                    </div>
+const DashboardLayout = ({ children, activeMenu }) => {
+  const { user } = useContext(UserContext);
+  // What if it takse sometime to load a skeleton would be good
+  // if(!user) return <div>Loading...</div>
+  return (
+    <div className="">
+      <Navbar activeMenu={activeMenu}></Navbar>
+      {user && (
+        <div className="flex">
+          <div className="max-[1080px]:hidden">
+            <SideMenu activeMenu={activeMenu}></SideMenu>
+          </div>
 
-                <div className="grow mx-5">{children}</div>
-                </div>
-            )}
+          <div className="grow mx-5">{children}</div>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default DashboardLayout
+export default DashboardLayout;
