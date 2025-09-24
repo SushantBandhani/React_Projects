941492ff8ebd24edec982bd88a504bb989054de7
Sushant Bandhani <101317113+SushantBandhani@users.noreply.github.com>
2025-09-24 20:09:02 +0530

    Corrected auth API to use async/await

--- EcomCart/src/features/auth/authApi.js
+++ EcomCart/src/features/auth/authApi.js
@@ -1,37 +1,32 @@
-export function createUser(userData){
-    return new Promise(async(resolve)=>{
-        const response=await fetch("http://localhost:3000/users",{
-            method:'POST',
-            body:JSON.stringify(userData),
-            headers:{'content-type':'application/json'}
-    })
+export async function createUser(userData) {
+    const response = await fetch("http://localhost:3000/users", {
+        method: 'POST',
+        body: JSON.stringify(userData),
+        headers: { 'content-type': 'application/json' }
+    });
 
     if (!response.ok) {
         throw new Error("Failed to create user");
-      }
-      
-  
-    const data=await response.json();
-    // TODO: on server it will only return some info of user (not password)
-    resolve({data})
-})}
+    }
 
+    const data = await response.json();
+    return { data };
+}
 
-export function checkUser(loginInfo){
-    return new Promise(async(resolve,reject)=>{
-        const email = loginInfo.email;
-        const password = loginInfo.password;
-        const response=await fetch("http://localhost:3000/users?email="+email)
-        const data=await response.json()
+export async function checkUser(loginInfo) {
+    const { email, password } = loginInfo;
+    const response = await fetch(`http://localhost:3000/users?email=${email}`);
+    const data = await response.json();
 
-        console.log({data})
-        if (data.length) {
-          if (password === data[0].password) {
-            resolve({ data: data[0] });
-          } else {
-            reject({ message: 'Wrong credentials' });
-          }
+    console.log({ data });
+    
+    if (data.length) {
+        if (password === data[0].password) {
+            return { data: data[0] };
         } else {
-          reject({ message: 'User not found' });
+            throw new Error('Wrong credentials');
         }
-})}
+    } else {
+        throw new Error('User not found');
+    }
+}
