54223aad37fa172c1774a402fb079b7b5899cdf5
Sushant Bandhani <101317113+SushantBandhani@users.noreply.github.com>
2025-10-01 15:32:15 +0530

    Refactor index.js

--- Medium-Clone/Backend/src/index.ts
+++ Medium-Clone/Backend/src/index.ts
@@ -1,19 +1,29 @@
-import { Hono } from 'hono'
-import {userRouter} from './routes/user'
-import { blogRouter } from './routes/blog'
-import { cors } from 'hono/cors'
-
+import { Hono } from "hono";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blog";
+import { cors } from "hono/cors";
+import { verify } from "hono/jwt";
 
 const app = new Hono<{
-  Bindings:{
-    DATABASE_URL:string,
-    JWT_SECRET:string
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+app.use("/*", cors());
+app.use("api/v1/user", async (c, next) => {
+  const header = c.req.header("Authorization") || "";
+  const response = await verify(header, c.env.JWT_SECRET);
+  if (response.id) {
+    next();
+  } else {
+    c.status(403);
+    return c.json({ error: "Unauthorised" });
   }
-}>()
+});
 
-app.use("/*",cors())
+app.route("api/v1/user", userRouter);
+app.route("api/v1/blog", blogRouter);
 
-app.route("api/v1/user",userRouter)
-app.route("api/v1/blog",blogRouter)
-
-export default app
+export default app;
