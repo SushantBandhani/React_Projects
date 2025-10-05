1960fa89dc09e8eaafe33e4d487fd4baaefe9e18
Sushant Bandhani <101317113+SushantBandhani@users.noreply.github.com>
2025-10-05 21:51:47 +0530

    Updating main file and initialising brand and category routes

--- EcomCart/Backend/index.js
+++ EcomCart/Backend/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
 const { createProduct } = require("./controller/Product");
-const productRouters=require("./routes/Product")
+const productsRouter = require('./routes/Products');
+const categoriesRouter = require('./routes/Categories');
+const brandsRouter = require('./routes/Brands');
 
 async function main() {
   try {
@@ -26,7 +28,9 @@ server.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-server.use("/products", productRouters.router);
+server.use('/products', productsRouter.router);
+server.use('/categories', categoriesRouter.router);
+server.use('/brands', brandsRouter.router);
 
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
