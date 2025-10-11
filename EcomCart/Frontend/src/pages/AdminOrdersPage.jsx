517e60287ca1d29129111de08d9bc8989fc8e5a8
Sushant Bandhani <101317113+SushantBandhani@users.noreply.github.com>
2025-10-11 12:34:19 +0530

    Adding Admin order page

---
+++ EcomCart/Frontend/src/pages/AdminOrdersPage.jsx
@@ -0,0 +1,9 @@
+import AdminOrders from "../features/admin/components/AdminOrder.jsx";
+
+export default function AdminOrdersPage(){
+return(
+    <div>
+        <AdminOrders></AdminOrders>
+    </div>
+)
+}
