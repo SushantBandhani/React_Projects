914bbb99d77dfb1319e81fee404a3fa2ad0780a7
Sushant Bandhani <101317113+SushantBandhani@users.noreply.github.com>
2025-10-22 21:00:00 +0530

    Updated main file and added models for cart and orders

--- EcomCart/Backend/index.js
+++ EcomCart/Backend/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
-const { createProduct } = require("./controller/Product");
 const productsRouter = require('./routes/Products');
 const categoriesRouter = require('./routes/Categories');
 const brandsRouter = require('./routes/Brands');
-
+const userRouter=require('./routes/User')
+const authRouter=require('./routes/Auth')
+const cartRouter=require('./routes/Cart')
+const orderRouter=require('./routes/Order')
+const cors=require("cors")
 async function main() {
   try {
     await mongoose.connect("mongodb://localhost:27017/ecomcart", {
@@ -21,6 +24,11 @@ async function main() {
 main().catch((err) => console.log(err));
 
 //middlewares
+server.use(cors(
+  {
+    exposedHeaders:['X-Total-Count']
+  }
+))
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
@@ -31,6 +39,10 @@ server.get("/", (req, res) => {
 server.use('/products', productsRouter.router);
 server.use('/categories', categoriesRouter.router);
 server.use('/brands', brandsRouter.router);
+server.use('/users', userRouter.router);
+server.use('/auth', authRouter.router);
+server.use('/cart', cartRouter.router);
+server.use('/order', orderRouter.router);
 
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
